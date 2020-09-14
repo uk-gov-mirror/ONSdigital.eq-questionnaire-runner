976ea38f9a204655075ee4a121949d5ebd9ee4d3
Mebin Abraham <mebin95+work@gmail.com>
2020-09-14 12:38:57 +0100

    Add support for webserver work class

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,7 +3,7 @@ import os
 import gunicorn
 
 workers = os.getenv("WEB_SERVER_WORKERS")
-worker_class = "gevent"
+worker_class = os.getenv("WEB_SERVER_WORKER_CLASS")
 keepalive = os.getenv("GUNICORN_KEEP_ALIVE")
 bind = "0.0.0.0:5000"
 gunicorn.SERVER_SOFTWARE = "None"
