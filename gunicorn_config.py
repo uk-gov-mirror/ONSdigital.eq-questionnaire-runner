ef3272637e4e9c71409e9b4ca14e6f919e1c4ef0
Mebin Abraham <mebin95+work@gmail.com>
2020-09-18 14:15:22 +0100

    Ability to set gunicorn worker threads

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,6 +4,7 @@ import gunicorn
 
 workers = os.getenv("WEB_SERVER_WORKERS")
 worker_class = os.getenv("WEB_SERVER_WORKER_CLASS")
+threads = os.getenv("WEB_SERVER_THREADS")
 keepalive = os.getenv("GUNICORN_KEEP_ALIVE")
 bind = "0.0.0.0:5000"
 gunicorn.SERVER_SOFTWARE = "None"
